401b3c260c5b2362808d5631f34a5c31ced00b31
Andres Quintero <aquintero@oru.com.co>
2018-08-23 12:04:27 -0500

    Added output function

--- lib/output.sh
+++ lib/output.sh
@@ -5,15 +5,15 @@
 # }
 
 # # format output and send a copy to the log
-# output() {
-#   local logfile="$1"
+output() {
+  local logfile="$1"
 
-#   while read LINE;
-#   do
-#     echo "       $LINE" || true
-#     echo "$LINE" >> "$logfile" || true
-#   done
-# }
+  while read LINE;
+  do
+    echo "       $LINE" || true
+    echo "$LINE" >> "$logfile" || true
+  done
+}
 
 # header() {
 #   echo "" || true
